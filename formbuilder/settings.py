18f276245100187d56beb9d569a22832ee08b993
jamshi <jamshi.onnet@gmail.com>
2017-09-02 23:45:55 +0530

    settings for heroku

--- formbuilder/settings.py
+++ formbuilder/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%1-nhji@euq3v@5jg&(l$ryvqu5%f%lu7kj(m(g9%i#7+(o%dp'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
