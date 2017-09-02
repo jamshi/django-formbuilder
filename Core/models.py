65e97e7164527d3ec1ed10e8ebcccc4dd6356378
jamshi <jamshi.onnet@gmail.com>
2017-09-03 00:25:00 +0530

    settings for heroku

--- Core/models.py
+++ Core/models.py
@@ -5,6 +5,10 @@ from django.db import models
 # Create your models here.
 
 class MyForms(models.Model):
+
+    def __unicode__(self):
+        return u'%s' % self.name
+
     name = models.CharField(max_length=250, verbose_name='Form Name')
     configuration = models.TextField(blank=True, null=True)
     form_location = models.CharField(max_length=700, verbose_name='File Location')
